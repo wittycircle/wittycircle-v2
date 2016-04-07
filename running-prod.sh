d2e70ca696fc2387d8c225141269872de1881c51
root <root@ip-172-31-7-42.eu-west-1.compute.internal>
2016-04-07 18:08:55 +0000

    update

--- running-prod.sh
+++ running-prod.sh
@@ -4,6 +4,7 @@
 
 echo Running the script for landing the forever script...
 echo Shutting down the previous instance of server.js production
+# :TODO this is not good, i kill all process waiting to find a way to kill a process if he exist
 forever stop production
 echo Starting the server.js with forever and gloabl parameters at production.json...
 forever start production.json

bad466986662401f913799c04d4363ebb8d914bc
Chau Nguyen HO <JayHo@iMac-de-Chau.local>
2016-04-26 21:48:11 +0200

    update

--- Public/app/controllers/head.js
+++ Public/app/controllers/head.js
@@ -21,13 +21,11 @@ angular.module('wittyApp').controller('HeadCtrl', function ($scope) {
 		image: "https://res.cloudinary.com/dqpkpmrgk/image/upload/v1458576687/Share_banner_cover/banner_share.png"
 	};
 
-	if (window.innerWidth <= 736) {
+	if ($(window).width() <= 736) {
 		$scope.mobile = true;
-		$scope.viewportVal = "width=device-width,height=device-height,user-scalable=no,initial-scale=1.0";
+		$scope.viewport = "width=device-width,height=device-height,user-scalable=no,initial-scale=1.0";
 	} else {
 		$scope.mobile = false;
-		$scope.viewportVal = "width=device-width";
+		$scope.viewport = "width=device-width";
 	}
-
-	
 });

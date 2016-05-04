4b4124d7e229d94a2d477bf7f875aac60b1dab0d
mmasson01 <mmasson@student.42.fr>
2016-05-04 12:45:41 +0200

    ok

--- io.js
+++ io.js
@@ -158,7 +158,7 @@ module.exports = function(app, io, ensureAuth) {
                                                                     "type": "to"
                                                                 }],
                                                                 "headers": {
-                                                                    "Reply-To": "message.reply@example.com"
+                                                                    "Reply-To": "noreply@wittycircle.com"
                                                                 },
                                                                 "important": false,
                                                                 "inline_css": null,

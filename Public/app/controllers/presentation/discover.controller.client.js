8cecfb60ff4342664335eae6e4d4f2456c92b0e2
root <root@ip-172-31-7-42.eu-west-1.compute.internal>
2016-05-10 09:27:22 +0000

    o

--- Public/app/controllers/presentation/discover.controller.client.js
+++ Public/app/controllers/presentation/discover.controller.client.js
@@ -480,7 +480,7 @@ angular.module('wittyApp')
                     geo 	: value[4]
                 };
                 if (value[0] || value[1] || value[4]) {
-                    $state.transitionTo('discover', {status, value[0], category: value[1], location: value[4]}, { notify: false, inherit: true });
+                    $state.transitionTo('discover', {status: value[0], category: value[1], location: value[4]}, { notify: false, inherit: true });
                     return searchScl(object);
                 }
             }

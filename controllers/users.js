e84fb88c9a913bfc2255a229d1922f3fd79035ab
root <root@ip-172-31-17-115.eu-west-1.compute.internal>
2016-10-24 20:32:21 +0000

    update

--- controllers/users.js
+++ controllers/users.js
@@ -239,11 +239,11 @@ exports.getCardProfilePlus = function(req, res) {
                 });
             });
     } else
-        res.status(400).send("Error data!");
+        return res.status(400).send("Error data!");
 };
 
 exports.getCardProfileHome = function(req, res) {
-    console.log(req.body);
+
     req.checkBody('ip', "error").isString();
 
     var errors = req.validationErrors(true);

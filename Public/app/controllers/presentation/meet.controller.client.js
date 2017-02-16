f92bbb0edb97412093782b4fe28b20db76d3eb7e
root <root@ip-172-31-17-115.eu-west-1.compute.internal>
2017-02-16 16:00:01 +0000

    update

--- Public/app/controllers/presentation/meet.controller.client.js
+++ Public/app/controllers/presentation/meet.controller.client.js
@@ -48,7 +48,7 @@ angular.module('wittyApp').controller('MeetCtrl', function($filter, Picture, $st
 
 	$scope.$parent.card = {
 		title: "Wittycircle | Meet",
-		url: "http://127.0.0.1/meet",
+		url: "https://www.wittycircle.com/meet",
 		image: "https://res.cloudinary.com/dqpkpmrgk/image/upload/v1465994773/Share_Link_Cards_Facebook/Share_Pic_Facebook_Meet.png",
 	};
 

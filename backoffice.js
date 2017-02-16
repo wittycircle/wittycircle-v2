98c7bb310ee99e883d777d6296bf1e1205a683d2
root <root@ip-172-31-17-115.eu-west-1.compute.internal>
2017-02-16 12:42:24 +0000

    update

--- backoffice.js
+++ backoffice.js
@@ -520,14 +520,14 @@ function run() {
 																																														   });
 																																									});
 														// run(); 
-														run1(); 
-														run2(); 
-														run3(); 
-														run4(); 
-														run5(); 
-														run6(); 
-														run7(); 
-														run8(); 
-														run9(); 
-														run10();																						
+
+												//		run2(); 
+												//		run3(); 
+												//		run4(); 
+												//		run5(); 
+												//		run6(); 
+												//		run7(); 
+												//		run8(); 
+												//		run9(); 
+												//		run10();																						
 };
